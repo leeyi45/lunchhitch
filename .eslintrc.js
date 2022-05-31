2ade481da195442dc4e7708b706e422d0f50435c
leeyi45 <leeyi45@gmail.com>
2022-05-31 21:42:49 +0800

    Added eslint configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,49 @@
+/**
+ * Configuration for eslint
+ */
 module.exports = {
-  "extends": ["next/core-web-vitals", "prettier", "eslint:recommended", "airbnb-typescript"],
-  "plugins": ["prettier", "react"]
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'eslint-plugin-import',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'linebreak-style': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/destructuring-assignment': 0,
+    'react/jsx-filename-extension': 0,
+    'react/no-array-index-key': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
