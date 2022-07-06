2c3c2de564677c149ae57d9116697d438bbbee8d
Branda <brandaang@gmail.com>
2022-07-06 10:20:21 +0800

    Update UserHomePage.tsx

--- src/pages/profile/user/UserHomePage.tsx
+++ src/pages/profile/user/UserHomePage.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 
-import { LunchHitchUser } from '../../../auth';
+// import { LunchHitchUser } from '../../../auth';
+import { useSession } from '../../../auth/auth_provider';
 import SwipeableEdgeDrawer from '../../../common/components/SwipeableEdgeDrawer/SwipeableEdgeDrawer';
 
 import styles from './UserHomePage.module.css';
 
-export default function UserHomePage({ user }: { user: LunchHitchUser }) {
+export default function UserHomePage() {
+  const { user } = useSession();
   return (
     <div className={styles.UserHomePage}>
       <Typography
         variant="h1"
         component="div"
       >
-        Welcome back {user.username}!
+        Welcome back {user!.username}!
       </Typography>
       <p style={{ fontSize: '30px' }}>What will it be today?
 
