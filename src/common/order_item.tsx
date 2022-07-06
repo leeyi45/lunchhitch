9642fae9dfe67463ef759bc8f11f2c88f7b65930
Branda <brandaang@gmail.com>
2022-07-06 09:46:07 +0800

    Update order_item.tsx

--- src/common/order_item.tsx
+++ src/common/order_item.tsx
@@ -4,6 +4,7 @@ import { Order } from '@prisma/client';
 export default function OrderItem({ order }: { order: Order }) {
   return (
     <>
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
       <h3>Order from Niqqi's</h3>
       <ol>
         {order.orders.map((item, i) => (
