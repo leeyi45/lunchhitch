16dc097a80b3b10be7871b09904f031d3d48a601
Branda <brandaang@gmail.com>
2022-07-22 11:17:06 +0800

    user home page update

--- src/pages/profile/user/index.tsx
+++ src/pages/profile/user/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Stack, Typography } from '@mui/material';
 
-import Box from '../../../common/components/Box';
 import SwipeableEdgeDrawer from '../../../common/components/SwipeableEdgeDrawer';
 
 import styles from './UserHomePage.module.css';
@@ -11,26 +10,12 @@ export default function UserHomePage() {
   return (
     <div className={styles.UserHomePage}>
       <Stack direction="column">
-        <Box style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '50%',
-          alignItems: 'center',
-          justifyContent: 'center',
-          position: 'absolute',
-          left: '25%',
-          top: '25%',
-          textAlign: 'center',
-          backgroundColor: 'rgba(152, 255, 152, 0.7)',
-        }}
+        <Typography
+          variant="h1"
+          style={{ fontSize: '100px', textShadow: '2px 8px 6px rgba(0,0,0,0.4), 0px -5px 35px rgba(255,255,255,1)', textAlign: 'center' }}
         >
-          <Typography
-            variant="h1"
-            style={{ fontSize: '100px', textShadow: '2px 8px 6px rgba(0,0,0,0.4), 0px -5px 35px rgba(255,255,255,0.4)' }}
-          >
-            Welcome back!
-          </Typography>
-        </Box>
+          Welcome back!
+        </Typography>
         <p style={{ fontSize: '30px' }}>What will it be today?
 
         </p>
